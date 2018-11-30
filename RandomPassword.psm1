e1767a3a8e2f524e6863fda7a90014c45b44c36b
Anthony Roud <anthoy.roud@amorgroup.local>
2018-11-30 08:53:50 +0000

    initial commit

---
+++ RandomPassword.psm1
@@ -0,0 +1,84 @@
+﻿# Function to return a specified number of non-alphanumeric characters
+function Get-NonAlphaChars {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=1)][int]$length,
+        [Parameter(Mandatory=$true,Position=2)][int]$ratio
+    )
+
+    $sublength = $length / $ratio
+
+    $no = 1..$sublength | Get-Random
+
+    $nonalphanumeric = '!£$%^&*()=+@#?'
+
+    $nonalphanumeric.ToCharArray() | Get-Random -Count $no
+}
+# Function to return a specified number of numeric chars
+function Get-NumericChars {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=1)][int]$length,
+        [Parameter(Mandatory=$true,Position=2)][int]$ratio
+    )
+    $sublength = $length / $ratio
+    $no = 1..$sublength | Get-Random
+
+    $numericChars = 0..9
+
+    $numericChars | Get-Random -Count $no
+}
+# Function to return a specified number of upper case chars
+function Get-UpperCaseChars {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=1)][int]$length,
+        [Parameter(Mandatory=$true,Position=2)][int]$ratio
+    )
+    $sublength = $length / $ratio
+
+    $no = 1..$sublength | Get-Random
+
+    $UpperCaseChars = (65..90) | ForEach-Object { [char]$_ }
+
+    $UpperCaseChars | Get-Random -Count $no
+}
+# Function to return a specified number of lower case chars
+function Get-LowerCaseChars {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=1)][int]$no
+    )
+
+    $lowerCaseChars = (97..122) | ForEach-Object { [char]$_ }
+
+    $lowerCaseChars | Get-Random -Count $no
+}
+Function Get-FullRandomPassword {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=1)][int]$length,
+        [Parameter(Mandatory=$true,Position=1)][int]$complexity
+    )
+
+    $lengthRemaining = $length
+
+    if($complexity -gt 3) {
+        $nonAlpha = Get-NonAlphaChars -length $length -ratio 4
+        $lengthRemaining = $lengthRemaining - $nonAlpha.count
+    }
+    if($complexity -gt 2) {
+        $NumericChars = Get-NumericChars -length $length -ratio 4
+        $lengthRemaining = $lengthRemaining - $NumericChars.count
+    }
+    if($complexity -gt 1) {
+        $UCChars = Get-UpperCaseChars -length $length -ratio 3
+        $lengthRemaining = $lengthRemaining - $UCChars.count
+    }
+
+    $LCChars = Get-LowerCaseChars -no $lengthRemaining
+
+    $finalPassWord = $nonAlpha + $NumericChars + $UCChars + $LCChars
+
+    ($finalPassWord | Get-Random -Count $finalPassword.Length) -join ''
+}
---
+++ newrandompassword.tests.ps1
@@ -0,0 +1,128 @@
+Import-Module 'C:\Users\anthony.roud\Desktop\Dev\RandomPassword.psm1' -force
+
+# Set Regex variables
+$LCRegex   = 'a-z'
+$UCRegex   = 'A-Z'
+$NUMRegex  = '0-9'
+$NANRegex = '!£$%^&*()=+@#?'
+$fullregex = $LCRegex,$UCRegex,$NUMRegex,$NANRegex
+
+Describe Get-NonAlphaChars {
+
+    [int]$length = 10
+    [int]$ratio = 3
+    [int]$listlen = $length / $ratio
+
+    $CharList = Get-NonAlphaChars -length $length -ratio $ratio
+
+    It "Returns a list of non alphanumeric characters" {
+        $CharList | ForEach-Object {
+            [regex]::match($_,$NANRegex) | Should -be $true
+        }
+    }
+    It "Should not contain any spaces" {
+        $CharList -match '\s' | Should -Be $false
+    }
+    It "Length of array returned should be between 1 and $listlen" {
+        $CharList.count -gt 0 -and $CharList.count -le $listlen | Should -be $true
+    }
+}
+Describe Get-UpperCaseChars {
+
+    [int]$length = 10
+    [int]$ratio = 2
+    [int]$listlen = $length / $ratio
+
+    $CharList = Get-UpperCaseChars -length $length -ratio $ratio
+
+    It "Returns a list of uppercase characters only" {
+        $CharList | ForEach-Object {
+            [regex]::match($_,$UCRegex) | Should -be $true
+        }
+    }
+    It "Should not contain any spaces" {
+        $CharList -match '\s' | Should -Be $false
+    }
+    It "Length of array returned should be between 1 and $listlen" {
+        $CharList.count -gt 0 -and $CharList.count -le $listlen | Should -be $true
+    }
+}
+Describe Get-NumericChars {
+
+    [int]$length = 10
+    [int]$ratio = 3
+    [int]$listlen = $length / $ratio
+
+    $CharList = Get-NumericChars -length $length -ratio $ratio
+
+    It "Returns a list of numbers only" {
+        $CharList | ForEach-Object {
+            [regex]::match($_,$CharList) | Should -be $true
+        }
+    }
+    It "Should not contain any spaces" {
+        $CharList -match '\s' | Should -Be $false
+    }
+    It "Length of array returned should be between 1 and $listlen" {
+        $CharList.count -gt 0 -and $CharList.count -le $listlen | Should -be $true
+    }
+}
+Describe Get-LowerCaseChars {
+
+    [int]$no = 5
+
+    $CharList = Get-LowerCaseChars -no $no
+
+    It "Returns a list of lower case letters only" {
+        $CharList | ForEach-Object {
+            [regex]::match($_,$LCRegex) | Should -be $true
+        }
+    }
+    It "Should not contain any spaces" {
+        $CharList -match '\s' | Should -Be $false
+    }
+    It "Length of array returned should be $no" {
+        $CharList.length | Should -beexactly $no
+    }
+}
+Describe Get-FullRandomPassword {
+
+    [int]$length = 10
+
+    for ($i=1; $i -lt 5; $i++){
+
+        $randomPassword = Get-FullRandomPassword -length $length -Complexity $i
+
+        $regexCombo = '[' + ($fullRegex[0..($i-1)] -join '') + ']'
+
+        It "Complexity $i returns password of length $length" {
+            $randomPassword.length | Should -beexactly $length
+        }
+        It "Should not contain any spaces" {
+            $CharList -match '\s' | Should -Be $false
+        }
+        It "Complexity $i should only include the correct character types" {
+            $randomPassword | ForEach-Object {
+                [regex]::match($_,$regexCombo) | Should -be $true
+            }
+        }
+        if($i -eq 3){
+            It "Complexity $i should not include the wrong character types" {
+                [regex]::matches($randomPassword,$NANRegex) | Should -be 0
+            }
+        }
+        elseif($i -eq 2){
+            It "Complexity $i should not include the wrong character types" {
+                [regex]::match($randomPassword,$NANRegex) | Should -be $false
+                [regex]::match($randomPassword,$NUMRegex) | Should -be $false
+            }
+        }
+        elseif($i -eq 1){
+            It "Complexity $i should not include the wrong character types" {
+                [regex]::match($randomPassword,$NANRegex) | Should -be $false
+                [regex]::match($randomPassword,$NUMRegex) | Should -be $false
+                [regex]::match($randomPassword,$UCRegex) | Should -be $false
+            }
+        }
+    }
+}
